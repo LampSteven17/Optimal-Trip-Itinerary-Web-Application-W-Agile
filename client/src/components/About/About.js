5e3ea2fb3a2e46c04eaa4da06c389106772de822
asoriano12 <asori@mabasori.com>
2020-02-02 12:40:07 -0700

    add biography to about page for Austin Soriano

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,26 +1,51 @@
 import React, {Component} from 'react';
-import {Container, Row, Col, Button} from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Container,
+  Row,
+  Col,
+  Button
+} from 'reactstrap';
 import {CLIENT_TEAM_NAME} from "../Constants";
 import '../tcowebstyle.css';
 
 export default class About extends Component {
 
-    render() {
-      return (
-        <Container id="about">
-          <Row>
-            <Col>
-              <h2 className="font-weight-bold" >
-                {CLIENT_TEAM_NAME}
-              </h2>
-            </Col>
-            <Col id="closeAbout" xs='auto' >
-              <Button className='btn-csu w-100' onClick={this.props.closePage} xs={1}>
-                Close
-              </Button>
-            </Col>
-          </Row>
-        </Container>
-      )
-    }
+  render() {
+    return (<Container id="about">
+      <Row>
+        <Col>
+          <h2 className="font-weight-bold">
+            {CLIENT_TEAM_NAME}
+          </h2>
+        </Col>
+        <Col id="closeAbout" xs='auto'>
+          <Button className='btn-csu w-100' onClick={this.props.closePage} xs={1}>
+            Close
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card style={{
+              width: '18rem'
+            }}>
+            <CardBody>
+              <CardTitle><strong>Austin Soriano</strong></CardTitle>
+              <CardText>
+                Hello my name is Austin. I am currently a Junior at Colorado
+                State University and Majoring in Computer Science. In my free
+                time I enjoy Snowboarding and Mountain Biking.
+              </CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>)
+  }
 }

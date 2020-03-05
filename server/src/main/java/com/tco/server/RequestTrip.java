cc7b885a34fae4360c33f0242e9d98f25c1592c9
cademccu <cademccu@rams.colostate.edu>
2020-03-04 20:53:01 -0700

    initial class build for Trip, without testing

--- server/src/main/java/com/tco/server/RequestTrip.java
+++ server/src/main/java/com/tco/server/RequestTrip.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import t10.server.src.main.java.com.tco.server.RequestDistance;
 
 
 /*
@@ -16,7 +17,8 @@ public class RequestTrip extends RequestHeader{
 
     private Map<String, String> options;
     private List<Map < String, String> > places;
-    private double[] distances;
+    private Long[] distances;
+    private Double earthRadius; // maybe some capital D Doubles here
 
 
 
@@ -32,18 +34,31 @@ public class RequestTrip extends RequestHeader{
     public void buildResponse() {
         if (distances.length < 1) {
             // Then we need to calculate
+            distances = calculateDistances(places);
         }
     }
 
-    private double[] calculateDistances(List< Map<String, String> > places) {
-        double[] dists = new double[places.size()];
+    private Long[] calculateDistances(List< Map<String, String> > places) {
+        Long[] dists = new Long[places.size()];
 
-            for (int i = 0; i < places.size(); i++){
-
+            for (int i = 0; i < places.size()-1; i++){
+                // check if name is null
+                if (places.get(i).get("name") == null) {
+                    return dists;
+                } else {
+                    dists[i] = RequestDistance.calculateDistance(places.get(i), places.get(i+1), earthRadius);
+                }
             }
+            if (dists.length > 1) {
+                // finish final entry
+                dists[dists.length-1] = RequestDistance.calculateDistance(places.get(0), places.get(places.size()-1), earthRadius);
+            }
+            // do last and first outside of loop
 
         return dists;
     }
+
+
 
 
 

a09a9991b22f31f8a8ae936e3286501339d697e9
asoriano12 <asori@mabasori.com>
2020-03-26 14:38:24 -0600

    add polyline tests

--- client/test/Atlas.test.js
+++ client/test/Atlas.test.js
@@ -64,73 +64,32 @@ function testGetPositionsOutput() {
 }
 
 function testPolyline() {
+  jest.mock('leaflet');
+  let testPolyAtlas = shallow(<Atlas />);
+
   let pointTest1 = [[0,0],[0,100]];
-  let expectedResult1 = [
-    {
-      "type": {},
-      "key": "1577959247181.1738",
-      "ref": null,
-      "props": {
-        "color": "red",
-        "positions": [[0, 0],[0, 100]]
-      },
-      "_owner": null,
-      "_store": {}
-    }
-  ];
-  let pointTest2 = [[0,140],[0,-20]];
-  let expectedResult2 = [
-    {
-      "type": {},
-      "key": "389479381985.7301",
-      "ref": null,
-      "props": {
-        "color": "red",
-        "positions": [[0, -140], [0, -310]]
-      },
-      "_owner": null,
-      "_store": {}
-    },
-    {
-      "type": {},
-      "key": "1431468603742.495",
-      "ref": null,
-      "props": {
-        "color": "red",
-        "positions": [[0, 220],[0, 50]]
-      },
-      "_owner": null,
-      "_store": {}
-    }
-  ];
+  let expectedResult1 = [[0, 0], [0, 100]];
+
+  let pointTest2 = [[0,140],[0,-70]];
+  let expectedResult2a = [[0, 290], [0, 140]];
+  let expectedResult2b = [[0, -70], [0, -220]];
+
   let pointTest3 = [[0, -160], [0, 70]];
-  let expectedResult3 = [
-    {
-      "type": {},
-      "key": "838607087921.1139",
-      "ref": null,
-      "props": {
-        "color": "red",
-        "positions": [[0, -160],[0, -290]]
-      },
-      "_owner": null,
-      "_store": {}
-    },
-    {  
-      "type": {},
-      "key": "1111724324699.8079",
-      "ref": null,
-      "props": {
-        "color": "red",
-        "positions": [[0, 200],[0, 70]]
-      },
-      "_owner": null,
-      "_store": {}
-    }
-  ];
+  let expectedResult3a = [[0, -160], [0, -290]];
+  let expectedResult3b = [[0, 200], [0, 70]];
+
+  let test1 = JSON.parse(JSON.stringify(testPolyAtlas.instance().generateLineArray(pointTest1)));
+  expect(test1[0].props.positions).toEqual(expectedResult1);
+  let test2 = JSON.parse(JSON.stringify(testPolyAtlas.instance().generateLineArray(pointTest2)));
+  expect(test2[0].props.positions).toEqual(expectedResult2a);
+  expect(test2[1].props.positions).toEqual(expectedResult2b);
+  let test3 = JSON.parse(JSON.stringify(testPolyAtlas.instance().generateLineArray(pointTest3)));
+  expect(test3[0].props.positions).toEqual(expectedResult3a);
+  expect(test3[1].props.positions).toEqual(expectedResult3b);
 }
 
 test("Testing Atlas's Initial State", testInitialAppState);
 test("Testing Atlas's Handle Input", testInitialHandleInput);
 test("Testing Atlas's Store Input Position",testStoreInputPosition);
 test("Testing Atlas's getPositions method",testGetPositionsOutput);
+test("Testing Atlas's polyline methods",testPolyline);

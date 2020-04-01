74724dad68544a238dea2e12ea3b829143b2c1ab
asoriano12 <asori@mabasori.com>
2020-04-01 12:51:06 -0600

    add dropdown

--- client/src/components/Atlas/Save.js
+++ client/src/components/Atlas/Save.js
@@ -1,37 +1,82 @@
 import React, {Component} from 'react';
 import '../tcowebstyle.css';
-import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+import {
+  Button,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from 'reactstrap';
+
+import {save} from 'save-file';
 
 class Save extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false
+      showModal: false,
+      showDropdown: false
     }
     this.toggleModal = this.toggleModal.bind(this);
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+
   }
 
   render() {
-    return(
-      <div>
-        <Button size="md" className={"btn-csu"} onClick={() => this.toggleModal()}>Save Itinerary</Button>
-        <Modal isOpen={this.state.showModal} toggle={() => this.toggleModal()}>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </ModalBody>
-          <ModalFooter>
-           <Button color="primary" onClick={() => this.toggleModal()}>Save</Button>{' '}
-           <Button color="secondary" onClick={() => this.toggleModal()}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    )
+    return (<div>
+      <Button size="md" className={"btn-csu"} onClick={() => this.toggleModal()}>Save Itinerary</Button>
+      <Modal isOpen={this.state.showModal} toggle={() => this.toggleModal()}>
+        <ModalHeader>
+          Save Map
+        </ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label>Name</Label>
+              <Input id="saveName" placeholder="filename"></Input>
+            </FormGroup>
+            <FormGroup>
+              <Label>File Type</Label>
+              <Dropdown isOpen={this.state.showDropdown} toggle={() => this.toggleDropdown()}>
+                <DropdownToggle caret>
+                  Type
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>.json</DropdownItem>
+                  <DropdownItem>.csv</DropdownItem>
+                  <DropdownItem>.kml</DropdownItem>
+                  <DropdownItem>.svg</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={() => this.toggleModal()}>Save</Button>{' '}
+          <Button color="secondary" onClick={() => this.toggleModal()}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>)
   }
 
   toggleModal() {
-      this.setState({
-       showModal: !this.state.showModal
-     })
+    this.setState({
+      showModal: !this.state.showModal
+    });
+  }
+
+  toggleDropdown() {
+    this.setState({
+      showDropdown: !this.state.showDropdown
+    });
   }
 }
 

cd1a68b4dd809458735dd9fa1cdc0270fbee30ff
cademccu <cademccu@rams.colostate.edu>
2020-04-15 18:36:50 -0600

    add tests, fix imports, resolve #296

--- server/src/test/java/com/tco/server/TestRequestTrip.java
+++ server/src/test/java/com/tco/server/TestRequestTrip.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.json.JSONObject;
-import org.json.JSONArray;
 
 import static org.junit.Assert.assertEquals;
 

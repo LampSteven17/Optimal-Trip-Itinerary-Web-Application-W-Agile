945408724b64325ee20cd509e5e51e059ba4c206
cademccu <cademccu@rams.colostate.edu>
2020-04-29 20:19:42 -0600

    fully implement json structures

--- server/src/main/java/com/tco/server/Find.java
+++ server/src/main/java/com/tco/server/Find.java
@@ -8,17 +8,35 @@ import java.util.*;
 
 public class Find extends RequestHeader {
 
-
     private String match;
     private int limit;
     private int found;
     private List<Map<String, String> > places;
+    private Narrow narrow;
 
     @Override
     public void buildResponse() throws IOException {
-        System.out.println(this.places.size() + " ==============");
+        // Find something
     }
 
+    // quick data structure for narrow cause why not
+    protected class Narrow{
+        private List<String> type;
+        private String where;
+
+        protected Narrow(List<String> type, String where) {
+            this.type = type;
+            this.where = where;
+        }
+
+        protected String getWhere() { return this.where != null ? this.where : null; }
+
+        protected String[] getTypes() {
+            if (this.type == null) return null;
+            return this.type.toArray(new String[this.type.size()]);
+        }
+
+    }
 
 
 }

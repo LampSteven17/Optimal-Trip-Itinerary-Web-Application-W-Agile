424502818b6bf411a935500f7785df2f3842a8c8
cademccu <cademccu@rams.colostate.edu>
2020-05-01 17:43:30 -0600

    followed daves steps for adding travis and DB's

--- server/src/main/java/com/tco/server/DataBaseAccessor.java
+++ server/src/main/java/com/tco/server/DataBaseAccessor.java
@@ -13,12 +13,14 @@ public class DataBaseAccessor {
     // strings for either home computer or sprint deployment
     private final static String HOME_DEV_DB_URL = "jdbc:mysql://127.0.0.1:56247/cs314";
     private final static String DEPLOY_DB_URL = "jdbc:mysql://faure.cs.colostate.edu/cs314";
-
+    private final static String TRAVIS_DB_URL = "jdbc:mysql://127.0.0.1/cs314";
     private String DB_URL = DEPLOY_DB_URL;
-
-    // shared user with read-only access
-    private final static String DB_USER = "cs314-db";
-    private final static String DB_PASSWORD = "eiK5liet1uej";
+    // username or password via environment
+    private final static String DB_TRAVIS = "root";
+    private final static String DB_HOME_OR_DEPLOY = "root";
+    private final static String DB_PASSWORD_HOME_OR_DEPLOY = "eiK5liet1uej";
+    private  String DB_USER = DB_HOME_OR_DEPLOY;
+    private String DB_PASSWORD = DB_PASSWORD_HOME_OR_DEPLOY;
 
     // SQL SELECT query statement
     private final static String COLUMN = "id";
@@ -30,12 +32,24 @@ public class DataBaseAccessor {
 
     // set export CS314_ENV=development
     private void set_URL_based_on_environment() {
-        String environment = System.getenv("CS314_ENV");
-        if (environment == null)
-            return;
+        // environment variables
+        String environment_home = System.getenv("CS314_ENV");
+        String environment_TRAVIS = System.getenv("TRAVIS");
 
-        if (environment.equals("development"))
-            DB_URL = HOME_DEV_DB_URL;
+        if (environment_TRAVIS != null) {
+
+            if (environment_TRAVIS.equals("true")) {
+                DB_URL = TRAVIS_DB_URL;
+                DB_USER = DB_TRAVIS;
+                DB_PASSWORD = null;
+            }
+        } else if (environment_home != null) {
+            if (environment_home.equals("development")) {
+                DB_URL = HOME_DEV_DB_URL;
+            }
+        } else {
+            DB_URL = DEPLOY_DB_URL;
+        }
     }
 
     // TODO this is gonna need quite a bit of modification to do intended stuff
